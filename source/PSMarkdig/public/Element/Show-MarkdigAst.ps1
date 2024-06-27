72eccb5213353911031bf3a1b2d49798418312a7
Timothy Aldrich <timothy.r.aldrich@gmail.com>
2024-06-27 15:43:09 -0400

    fix: Show-MarkdigAst endlessly displays first object

--- source/PSMarkdig/public/Element/Show-MarkdigAst.ps1
+++ source/PSMarkdig/public/Element/Show-MarkdigAst.ps1
@@ -11,6 +11,7 @@ function Show-MarkdigAst {
     param(
         # The markdig object to show
         [Parameter(
+            ValueFromPipeline
         )]
         [MarkdownObject]$Element,
 
@@ -28,22 +29,24 @@ function Show-MarkdigAst {
         Write-Debug "`n$('-' * 80)`n-- Begin $($MyInvocation.MyCommand.Name)`n$('-' * 80)"
     }
     process {
+        Write-Debug "Index is $index, Indent is $indent"
         $type = $Element.GetType().FullName
         "$(' ' * $indent)[$index] - $type"
         $index++
-        $count = (Write-Output $Element -NoEnumerate | Select-Object -ExpandProperty Count)
+        $count = Write-Output $Element -NoEnumerate | Select-Object -ExpandProperty Count
         Write-Debug "$type : has $count elements"
-        for ($i=0;$i -ge $count;$i++) {
-            "$(' ' * $indent)[$i] - $type"
-
-            $Element | ForEach-Object {
-                $indent++
-                $index++
-                Show-MarkdigAst $_ -index $index -indent $indent
+        $currentIndent = $indent
+        $currentIndex  = $index
+        if ($count -gt 1) {
+            $indent++
+            for ($i = 0; $i -lt $count; $i++) {
+                Show-MarkdigAst $Element[$i] -index $i -indent $indent
             }
         }
-}
-end {
-    Write-Debug "`n$('-' * 80)`n-- End $($MyInvocation.MyCommand.Name)`n$('-' * 80)"
-}
+        $indent = $currentIndent
+        $index  = $currentIndex
+    }
+    end {
+        Write-Debug "`n$('-' * 80)`n-- End $($MyInvocation.MyCommand.Name)`n$('-' * 80)"
+    }
 }
